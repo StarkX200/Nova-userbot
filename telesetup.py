d557f505dec77245cbe9ba40ac401462795507d0
StarkX200 <89933807+StarkX200@users.noreply.github.com>
2021-09-06 13:25:07 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -1,34 +1,12 @@
-from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 
-print("")
-print("")
-print(""" Welcome To Nova userbot String Generator...""")
-print("""Kindly Enter Your Details To Continue ! """)
+print("""Please go-to my.telegram.org
+Login using your Telegram account
+Click on API Development Tools
+Create a new application, by entering the required details""")
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
 
-API_KEY = "1754367"
-API_HASH = "231b8cc6cca12ee51a85cf543321f476"
-while True:
-    try:
-        with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
-            print("")
-            session = client.session.save()
-            client.send_message(
-                "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @jarvissupportofficial For Any Help !",
-            )
-            print(
-                "You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages "
-            )
-            print("Store it safe !!")
-            print(
-                "Thanks For Choosing NOVA USERNOT. Hope that You will Have a Goodtime With Us!....."
-            )
-    except BaseException:
-        print("")
-        print(
-            "Wrong phone number \n make sure its with correct country code. Example : +919961998999 ! Kindly Retry"
-        )
-        print("")
-        continue
-    break
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
