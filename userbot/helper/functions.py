897daf78f239795267f8c48d949fc6f027a2f5ff
STARK <89933807+StarkX200@users.noreply.github.com>
2021-09-15 15:05:55 +0530

    Update functions.py

--- userbot/helper/functions.py
+++ userbot/helper/functions.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 from asyncio import sleep
 from random import choice
 from telethon import events
-from PIL import Image
-from validators.url import url
+from emoji import get_emoji_regexp
 from telethon.tl.types import Channel
 
 
@@ -28,33 +27,3 @@ async def get_readable_time(seconds: int) -> str:
     time_list.reverse()
     up_time += ":".join(time_list)
     return up_time
-
-
-async def darkmusic(dark , QUALITY):
-  search = dark
-  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
-  soup = BeautifulSoup(html, 'html.parser')
-  for link in soup.find_all('a'):
-    if '/watch?v=' in link.get('href'):
-        # May change when Youtube Website may get updated in the future.
-        video_link = link.get('href') 
-        break
-  video_link =  'http://www.youtube.com/'+video_link
-  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)	
-  os.system(command)
-
-
-async def darkmusicvideo(dark):
-    search = dark
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-    html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
-    soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
-        if '/watch?v=' in link.get('href'):
-            # May change when Youtube Website may get updated in the future.
-            video_link = link.get('href') 
-            break    
-    video_link =  'http://www.youtube.com/'+video_link
-    command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  
-    os.system(command)
