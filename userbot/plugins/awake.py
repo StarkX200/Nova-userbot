b516abaa2a5ee519aa55ad753f8bf21f86d13776
STARK <89933807+StarkX200@users.noreply.github.com>
2021-09-15 16:16:23 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -1,8 +1,7 @@
 """Check if userbot awake or not . 
 
 """
-#make by @LEGENDX22 don't kang this plugin
-# if you kang then keep credits
+
 import os
 import time
 import asyncio
@@ -22,25 +21,24 @@ from platform import python_version, uname
 
 ALIVE_PHOTTO = Config.ALIVE_PHOTTO
 if ALIVE_PHOTTO is None:
-  ALIVE_PHOTTO = "https://telegra.ph/file/0e36b02061064b7229e3b.jpg"
+  ALIVE_PHOTTO = "https://telegra.ph/file/fc22c880e9b4c74b9f33f.jpg"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND BOY"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TSF USER"
 
 global ghanti
 tag = borg.uid
-ALIVE_MESSAGE= " ⚡ LEGEND BOT ⚡  IS ON 🔥 FIRE ⚜️ \n\n"
+ALIVE_MESSAGE= " ⚡ TSF ⚡  IS ON 🔥 FIRE ⚜️ \n\n"
 ALIVE_MESSAGE += "🔱 SYSTEM STATUS🔱\n"
-ALIVE_MESSAGE += "🔱TELETHON VERSION🔱 : 6.0.9\n⭕ Python: 3.7.4\n"
+ALIVE_MESSAGE += "🔱TELETHON VERSION🔱 : 6.0.9\n⭕ Python: 3.9.2\n"
 ALIVE_MESSAGE += "🔱DATABASE STATUS🔱 : Functional\n"
 ALIVE_MESSAGE += "🔱Current Branch🔱 : Master\n"
-ALIVE_MESSAGE += "🔱LEGEND OS🔱 :   `2.2.5`\n"
+ALIVE_MESSAGE += "🔱TSF OS🔱 :   `1.0`\n"
 #op Bolte uptime add krna h
 ALIVE_MESSAGE += f"⚜️ MY BOSS ⚜️: [{DEFAULTUSER}](tg://user?id={tag})\n"
-ALIVE_MESSAGE += "⚜️MADE BY⚜️ 😎 : [LEGEND X](https://t.me/legendx22)\n\n"
-ALIVE_MESSAGE += ":⚜️Deploy⚜️ **LEGEND BOT** : [ᖇᏋᎵᏫ](https://github.com/legendx22/LEGEND-BOT)\n"           
-#make by LEGEND X bht mehnat lag gayi yrr but banhi gaya 😅           
-#@command(outgoing=True, pattern="^.awake$")
+ALIVE_MESSAGE += "⚜️MADE BY⚜️ 😎 : [STARK](https://t.me/CAD_BALY)\n\n"
+ALIVE_MESSAGE += ":⚜️Deploy⚜️ **TSF BOT** : [ᖇᏋᎵᏫ](https://github.com/StarkX200/TSF-USERBOT)\n"           
+
 @borg.on(admin_cmd(pattern=r"awake"))
 async def amireallyalive(awake):
    """ For .awake command, check if the bot is running.  """
